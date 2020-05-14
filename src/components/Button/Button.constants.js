11cefc1d1ade1adcaf3fa8d6cb1b25ce09f9754e
yoavka <yoav.kadosh@amdocs.com>
2020-05-15 01:02:31 +0300

    Added button types/sizes

---
+++ src/components/Button/Button.constants.js
@@ -0,0 +1,9 @@
+export const Sizes = {
+    SMALL: 'SMALL',
+    LARGE: 'LARGE',
+};
+
+export const Types = {
+    PRIMARY: 'PRIMARY',
+    SECONDARY: 'SECONDARY',
+};
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {node, oneOf} from 'prop-types';
+import {Sizes, Types} from "./Button.constants";
 import './Button.scss';
 
-const Button = ({children, size, ...props}) => (
-    <div className={`button size-${size}`} {...props}>{children}</div>
+const Button = ({children, size, type, ...props}) => (
+    <div className={`button size-${size.toLowerCase()} type-${type.toLowerCase()}`} {...props}>{children}</div>
 );
+
+Button.propTypes = {
+    children: node,
+    size: oneOf(Object.values(Sizes)),
+    type: oneOf(Object.values(Types)),
+};
+
+Button.defaultProps = {
+    children: null,
+    size: Sizes.SMALL,
+    type: Types.PRIMARY,
+};
 
 export default Button;
