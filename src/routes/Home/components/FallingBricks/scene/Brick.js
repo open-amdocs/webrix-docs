6b18f899af247c94291fe093cdbe4bcefd5e64cc
yoavka <yoav.kadosh@amdocs.com>
2020-05-15 00:23:18 +0300

    Loading a custom .stl mesh for the bricks :)

--- src/routes/Home/components/FallingBricks/scene/Brick.js
+++ src/routes/Home/components/FallingBricks/scene/Brick.js
@@ -1,9 +1,20 @@
-export default scene => {
-    const brick = BABYLON.MeshBuilder.CreateBox("brick", {height: 1, width: 5, depth: 2}, scene);
-    brick.position = new BABYLON.Vector3(Math.random() *10 - 5, 30, Math.random() *10 - 5);
+const Material = scene => {
+    const material = new BABYLON.StandardMaterial("brick", scene);
+    // material.diffuseTexture = new BABYLON.Texture(`resources/images/${name}.jpg`, scene);
+    material.diffuseColor = new BABYLON.Color3.FromHexString('#e43f5a');
+    material.specularColor = new BABYLON.Color3(0, 0, 0);
+    material.backFaceCulling = false;
+    return material;
+};
+
+export default (scene, mesh) => {
+    const material = new Material(scene);
+    const brick = mesh.clone('brick');
+    brick.position = new BABYLON.Vector3(Math.random() *10 - 5, 30, Math.random() * 10 - 5);
     brick.rotation.y = Math.PI/(Math.random() * 2);
     brick.rotation.x = Math.PI/(Math.random());
     brick.physicsImpostor = new BABYLON.PhysicsImpostor(brick, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 20, friction: 0.4, restitution: 0.05}, scene);
+    brick.material = material;
 
     return brick;
 }
--- src/routes/Home/components/FallingBricks/scene/Scene.js
+++ src/routes/Home/components/FallingBricks/scene/Scene.js
@@ -10,6 +10,7 @@ const Scene = engine => {
     return scene;
 };
 
+// createScene(...):
 export default engine => {
     const scene = new Scene(engine);
     const camera = new Camera(scene);
@@ -18,29 +19,22 @@ export default engine => {
     const pipeline = new Pipeline(scene);
     const ground = new Ground(scene);
 
-    const materials = ['brick', 'brick1', 'brick2', 'brick3', 'brick4'].map(name => {
-        const material = new BABYLON.StandardMaterial("brick", scene);
-        // material.diffuseTexture = new BABYLON.Texture(`resources/images/${name}.jpg`, scene);
-        material.diffuseColor = new BABYLON.Color3.FromHexString('#e43f5a');
-        material.specularColor = new BABYLON.Color3(0, 0, 0);
-        material.backFaceCulling = false;
-        return material;
-    });
-
     // Add bricks every few seconds
     let prev = Date.now();
     const bricks = [];
-    scene.onBeforeRenderObservable.add(() => {
-        if (Date.now() - prev > 500) {
-            prev = Date.now();
-            const brick = new Brick(scene);
-            brick.material = materials[Math.floor(Math.random() * materials.length)];
-            shadows.addShadowCaster(brick);
-            bricks.push(brick);
-            if (bricks.length > 50) {
-                bricks.shift().dispose();
+    BABYLON.SceneLoader.ImportMesh('', 'resources/models/', 'brick.stl', scene, ([brickMesh]) => {
+        brickMesh.setEnabled(false); // Hide the original mesh
+        scene.onBeforeRenderObservable.add(() => {
+            if (Date.now() - prev > 500) {
+                prev = Date.now();
+                const brick = new Brick(scene, brickMesh);
+                shadows.addShadowCaster(brick);
+                bricks.push(brick);
+                if (bricks.length > 50) {
+                    bricks.shift().dispose();
+                }
             }
-        }
+        });
     });
 
     return scene;
