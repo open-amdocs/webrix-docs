64363242e4688b118eb29bafc1119036084e756e
yoavka <yoav.kadosh@amdocs.com>
2020-05-15 16:41:35 +0300

    Added point Light

--- src/routes/Home/components/FallingBricks/scene/Light.js
+++ src/routes/Home/components/FallingBricks/scene/Light.js
@@ -1,4 +1,6 @@
 export default scene => {
     const light = new BABYLON.DirectionalLight("dir02", new BABYLON.Vector3(15, -15, 15), scene);
+    const p = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(-20, 100, 0), scene);
+    p.intensity = 0.1;
     return light;
 };
