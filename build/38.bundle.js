a023c1e69bc87a02a5e3c432ab2a6d9dbfb27506
yoavka <yoav.kadosh@amdocs.com>
2020-06-24 21:00:06 +0300

    Created a snapshot build

---
+++ build/38.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{dGXR:function(n,e,o){"use strict";o.r(e),e.default=".movable-object {\n  user-select: none;\n  background-color: $color-pink;\n  width: 100px;\n  height: 100px;\n  cursor: move;\n  position: relative;\n  color: $color-white;\n  padding: 10px;\n  display: flex;\n  align-items: center;\n  text-align: center;\n}"}}]);
--- build/docs~625d070a.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{D1xp:function(n,c,e){"use strict";e.r(c);var t=e("cDcd"),o=e.n(t);c.default=function(){return o.a.createElement("div",null,"Docs")}}}]);
