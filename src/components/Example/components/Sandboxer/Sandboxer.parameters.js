b7ffc0485194e770ce30a4679ef79bc1f86ef3bd
yoavka <yoavks@gmail.com>
2020-10-21 14:28:26 +0300

    Taking webrix version from root package.json

--- src/components/Example/components/Sandboxer/Sandboxer.parameters.js
+++ src/components/Example/components/Sandboxer/Sandboxer.parameters.js
@@ -1,4 +1,5 @@
 import {getParameters} from 'codesandbox/lib/api/define';
+import rootpkg from '../../../../../package.json';
 import pkg from './files/package.json';
 import index from '!raw-loader!./files/index.js';
 import html from '!raw-loader!./files/index.html';
@@ -7,6 +8,9 @@ import colors from '!raw-loader!../../../../resources/styles/colors.scss';
 
 // Convert the SCSS variables to a JS object
 const _colors = [...colors.matchAll(/(\$[\w-]+): (#\w+)/g)].reduce((obj, [, name, value]) => ({...obj, [name]: value}), {});
+
+// Match the webrix version to the one in root
+pkg.dependencies.webrix = rootpkg.dependencies.webrix;
 
 export default ({code, id}) => getParameters({
     files: {
