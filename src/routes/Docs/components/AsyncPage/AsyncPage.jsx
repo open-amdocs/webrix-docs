4d0bdc8fd17cf8f6b487c22b917a1c625345de92
yoavka <yoavks@gmail.com>
2020-10-28 16:48:49 +0200

    Added "Edit This Page" button

--- src/routes/Docs/components/AsyncPage/AsyncPage.jsx
+++ src/routes/Docs/components/AsyncPage/AsyncPage.jsx
@@ -1,11 +1,17 @@
 import React, {Suspense, memo} from 'react';
+import {FaExternalLinkAlt} from 'react-icons/fa';
 import {Loader} from 'components';
+import './AsyncPage.scss';
 
 const AsyncPage = ({path, title}) => {
     const Comp = React.lazy(() => import(`../../content${path}/readme.mdx`));
+
     return (
         <article>
-            <h1>{title}</h1>
+            <h1>
+                {title}
+                <a target='_blank' rel='noreferrer' href={`https://github.com/open-amdocs/webrix-docs/blob/master/src/routes/Docs/content${path}/readme.mdx`}>Edit This Page <FaExternalLinkAlt/></a>
+            </h1>
             <Suspense fallback={<Loader/>}>
                 <Comp/>
             </Suspense>
