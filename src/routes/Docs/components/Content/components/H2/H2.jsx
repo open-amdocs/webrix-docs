34ccc42a2f47e2760293efd6211974ae242702bd
yoavka <yoavks@gmail.com>
2020-10-28 17:07:41 +0200

    Added Link icon to headings

--- src/routes/Docs/components/Content/components/H2/H2.jsx
+++ src/routes/Docs/components/Content/components/H2/H2.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {FaLink} from 'react-icons/fa';
 import {slugify} from 'utility';
 import './H2.scss';
 
 export default ({children}) => {
     const id = slugify(children);
     return (
-        <h2><a id={id} href={`#${id}`}>{children}</a></h2>
+        <h2><a id={id} href={`#${id}`}>{children}</a><FaLink/></h2>
     )
 }
