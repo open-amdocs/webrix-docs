77b369847fdbc9a9df2b9e13f82388669c806e5f
UDIA <udi.avganim@amdocs.com>
2020-11-05 18:49:56 +0200

    Lint fix

--- src/routes/Docs/content/components/scrollable/components/VerticalScrollbar/VerticalScrollbar.jsx
+++ src/routes/Docs/content/components/scrollable/components/VerticalScrollbar/VerticalScrollbar.jsx
@@ -1,23 +1,5 @@
-/**
- * Copyright (c) 2020, Amdocs Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import React from 'react';
-import {oneOfType, node, func, shape, instanceOf} from 'prop-types';
 import {Movable} from 'webrix';
-import {onUpdate} from './VerticalScrollbar.utils';
 import './VerticalScrollbar.scss';
 
 export default class VerticalScrollbar extends React.PureComponent {
