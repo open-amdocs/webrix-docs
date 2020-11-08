a6493e2076e22268ccf98e990a1d1b943b00f67a
yoavka <yoavks@gmail.com>
2020-11-08 13:32:18 +0200

    Fixed broken links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaNpm, FaGithub, FaCodeBranch, FaBug, FaStickyNote, FaCode} from 'react-icons/fa';
+import {FaNpm, FaGithub, FaCodeBranch, FaBug, FaCode} from 'react-icons/fa';
 import {Container} from '../';
 import Logo from './Components/Logo';
 import Copyright from './Components/Copyright';
@@ -40,7 +40,6 @@ const Footer = () => (
                     <li><FaGithub/><a href='https://github.com/open-amdocs/webrix'>GitHub</a></li>
                     <li><FaNpm/><a href='https://www.npmjs.com/package/webrix'>NPM</a></li>
                     <li><FaCodeBranch/><a href='https://github.com/open-amdocs/webrix/blob/master/CONTRIBUTING.md'>Contributing</a></li>
-                    <li><FaStickyNote/><a href='#'>Code of Conduct</a></li>
                     <li><FaBug/><a href='https://github.com/open-amdocs/webrix/issues'>Bugs</a></li>
                 </ul>
             </div>
