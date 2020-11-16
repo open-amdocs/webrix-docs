592535e1e5e3d45edcddbb27e47ea3810f9284a0
UDIA <udi.avganim@amdocs.com>
2020-11-16 17:53:16 +0200

    <Pannable> example

---
+++ src/routes/Docs/content/components/pannable/BasicExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Pannable} from 'webrix';
+import './BasicExample.scss';
+
+export default () => {
+    return (
+        <Pannable>
+            <div className='webrix-logo'></div>
+        </Pannable>
+    )
+};
