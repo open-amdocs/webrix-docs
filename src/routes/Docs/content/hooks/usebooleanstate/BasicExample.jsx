8b663202ba495fdc8b9a8cea93a7e772526d00e3
yoavka <yoavks@gmail.com>
2020-11-23 23:00:23 +0200

    Added useBooleanState docs

---
+++ src/routes/Docs/content/hooks/usebooleanstate/BasicExample.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import {useVisibilityState} from 'webrix/hooks';
+import './BasicExample.scss';
+
+export default () => {
+    const {visible, show, hide, toggle} = useVisibilityState(true);
+    return (
+        <div>
+            {visible && <div className='box'/>}
+            <div className='controls'>
+                <div onClick={show}>Show</div>
+                <div onClick={hide}>Hide</div>
+                <div onClick={toggle}>Toggle</div>
+            </div>
+        </div>
+    );
+};
