1d11f80a8ab56c9e352a42d455c0fb086a84bf73
yoavka <yoavks@gmail.com>
2021-02-08 12:02:50 +0200

    Upgraded to Webrix 1.1.2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaNpm, FaGithub, FaCodeBranch, FaBug, FaCode} from 'react-icons/fa';
+import {FaNpm, FaGithub, FaCodeBranch, FaBug, FaCode, FaTag} from 'react-icons/fa';
 import components from 'content/docs/components/components.routes';
 import hooks from 'content/docs/hooks/hooks.routes';
 import {Container} from '../';
@@ -45,6 +45,7 @@ const Community = () => (
         <h4>COMMUNITY</h4>
         <ul>
             <li><FaGithub/><a href='https://github.com/open-amdocs/webrix'>GitHub</a></li>
+            <li><FaTag/><a href='https://github.com/open-amdocs/webrix/releases'>Changelog</a></li>
             <li><FaNpm/><a href='https://www.npmjs.com/package/webrix'>NPM</a></li>
             <li><FaCodeBranch/><a href='https://github.com/open-amdocs/webrix/blob/master/CONTRIBUTING.md'>Contributing</a></li>
             <li><FaBug/><a href='https://github.com/open-amdocs/webrix/issues'>Bugs</a></li>
