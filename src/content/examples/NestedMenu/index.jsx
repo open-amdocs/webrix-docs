2b1ef482e49d1aa95bde64b5c0c4df47bc0bc593
UDIA <udi.avganim@amdocs.com>
2021-02-17 17:05:24 +0200

    Nested menu new structure

--- src/content/examples/NestedMenu/index.jsx
+++ src/content/examples/NestedMenu/index.jsx
@@ -1,57 +1,69 @@
-import React, {forwardRef, useRef, memo, useCallback} from 'react';
+import React, {useRef, useCallback} from 'react';
 import classNames from 'classnames';
 import {useVisibilityState, useClickOutside} from 'webrix/hooks';
 import {Poppable} from 'webrix/components';
 import {FaCut, FaCopy, FaEdit, FaFileImport, FaSave, FaTrashAlt, FaTrashRestore} from 'react-icons/fa';
 import './style.scss';
 
-const menuItems = [[{name: 'Edit', icon: FaEdit, showSubmenu: true},
-                   {name: 'Copy', icon: FaCopy},
-                   {name: 'Cut', icon: FaCut},
-                   {name: 'Import tasks', icon: FaFileImport},
-                   {name: '', divider: true},
-                   {name: 'Save as', icon: FaSave, color: 'green'},
-                   {name: '', divider: true},
-                   {name: 'Restore', icon: FaTrashRestore, color: 'salmon'},
-                   {name: 'Delete all', icon: FaTrashAlt, color: 'pink'}],
-                   [{name: 'Rename project'}, {name: 'Mark as seen'}, {name: 'Email notification'}, {name: 'Purchase', showSubmenu: true}],
-                   [{name: 'First item', showSubmenu: true}, {name: 'Second item'}, {name: 'Last item'}],
-];
-
-const MenuItem = forwardRef(({text, Icon, showTriangle, toggle, color}, ref) => (
-    text ?
-        (<div className={classNames('menu-item', color ? `color-${color}`: '')}>
-            <div className='content'>
-                {Icon && <Icon/>}
-            <span>{text}</span>
-            </div>
-            {showTriangle && <div className='triangle' onClick={toggle} ref={ref}></div>}
-        </div>) :
-        (<div className='menu-item-divider'/>)
-));
-
-const SubMenu = ({counter = 0}) => {
-    const {visible, toggle, hide} = useVisibilityState();
+const MenuItem = ({title, Icon, children, color}) => {
+    const {visible, show, hide} = useVisibilityState();
     const handleOnMouseDownCapture = useClickOutside(useCallback(hide, []));
-    const subMenuRef = useRef();
-    const {vcenter, hafter} = Poppable.Placements;
+    const child = children && React.Children.only(children);
+    const menuItemRef = useRef();
 
     return (
-        <div className='menu' onMouseDownCapture={handleOnMouseDownCapture}>
-            {visible &&
-            <Poppable placements={(rbr, tbr) => [{...hafter(rbr, tbr, 11), ...vcenter(rbr, tbr, 50)}]} reference={subMenuRef}>
-                <SubMenu counter={counter + 1}/>
-            </Poppable>
-            }
-            {menuItems[counter].map(({name, icon, showSubmenu, color}, index) =>
-                <MenuItem key={index} Icon={icon} text={name} toggle={showSubmenu && toggle} ref={showSubmenu && subMenuRef}
-                          showTriangle={showSubmenu && counter < 2} color={color}/>)}
+        <div className={classNames('menu-item', color ? `color-${color}` : '')} onMouseDownCapture={handleOnMouseDownCapture}
+             onClick={show} ref={menuItemRef}>
+            <div className='content'>
+                {Icon && <Icon/>}
+                <span>{title}</span>
+            </div>
+            {child && <div className='triangle'/>}
+            {visible && child && React.cloneElement(child, {reference: menuItemRef})}
         </div>
-
     );
 }
 
-export default memo(SubMenu);
+const Divider = () => <div className='menu-item-divider'/>;
+
+const Menu = ({children, reference}) => {
+    const {vcenter, hafter} = Poppable.Placements;
+
+    return (
+        <Poppable placements={(rbr, tbr) => [{...hafter(rbr, tbr, 11), ...vcenter(rbr, tbr, 50)}]} reference={reference}>
+            <div className='menu'>
+                {children}
+            </div>
+        </Poppable>
+    );
+}
+export default () => (
+    <Menu reference={new DOMRect((window.innerWidth - 170) / 2 ,(window.innerHeight - 200) / 2, 0 , 0)}>
+        <MenuItem title='Edit' Icon={FaEdit}>
+            <Menu>
+                <MenuItem title='Rename project'/>
+                <MenuItem title='Mark as seen'/>
+                <MenuItem title='Email notification'/>
+                <Divider/>
+                <MenuItem title='Purchase'>
+                    <Menu>
+                        <MenuItem title='First item'/>
+                        <MenuItem title='Second item'/>
+                        <MenuItem title='Last item'/>
+                    </Menu>
+                </MenuItem>
+            </Menu>
+        </MenuItem>
+        <MenuItem title='Copy' Icon={FaCopy}/>
+        <MenuItem title='Cut' Icon={FaCut}/>
+        <MenuItem title='Import tasks' icon={FaFileImport}/>
+        <Divider/>
+        <MenuItem title='Save as' Icon={FaSave} color='green'/>
+        <Divider/>
+        <MenuItem title='Restore' Icon={FaTrashRestore} color='salmon'/>
+        <MenuItem title='Delete all' Icon={FaTrashAlt} color='pink'/>
+    </Menu>
+)
 
 
 
