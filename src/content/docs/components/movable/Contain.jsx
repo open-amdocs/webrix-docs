c88198b458beb4373a2548673a81cda9e003dc65
yoavka <yoavks@gmail.com>
2021-02-24 02:42:50 +0200

    Updated readme, added "mixing" example

---
+++ src/content/docs/components/movable/Contain.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useRef} from 'react';
+import {Movable} from 'webrix/components';
+import './Contain.scss';
+
+export default () => {
+    const [position, onMove] = useState({});
+    const movable = useRef();
+    const container = useRef();
+    const {contain} = Movable.Constraints;
+    const props = Movable.useMove({ref: movable, onMove, constraints: [contain(container)]});
+
+    return (
+        <>
+            <div className='container' ref={container}/>
+            <Movable {...props} style={position}>
+                Move Me!
+            </Movable>
+        </>
+    );
+}
--- src/content/docs/components/movable/Mixing.jsx
+++ src/content/docs/components/movable/Mixing.jsx
@@ -1,19 +1,23 @@
 import React, {useState, useRef} from 'react';
 import {Movable} from 'webrix/components';
-import './Contained.scss';
+import './Mixing.scss';
 
 export default () => {
     const [position, onMove] = useState({});
     const movable = useRef();
     const container = useRef();
-    const {contain} = Movable.Constraints;
-    const props = Movable.useMove({ref: movable, onMove, constraints: [contain(container)]});
+    const {snap, contain} = Movable.Constraints;
+    const props = Movable.useMove({
+        ref: movable,
+        onMove,
+        constraints: [snap(1, 25), contain(container)]
+    });
 
     return (
         <>
             <div className='container' ref={container}/>
             <Movable {...props} style={position}>
-                Move Me!
+                I snap to a 1x25 grid
             </Movable>
         </>
     );
