02bc0500148644ea83ec2a535f7d1ace3703865b
yoavka <yoavks@gmail.com>
2021-03-05 14:25:31 +0200

    Only load iframe when visible (fixed #16)

--- src/components/Example/Example.jsx
+++ src/components/Example/Example.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useRef, memo}  from 'react';
+import React, {useState, useCallback, useRef, useEffect, memo}  from 'react';
 import cls from 'classnames';
 import Highlighter from '../Highlighter';
 import Controls from './components/Controls/Controls';
@@ -10,12 +10,27 @@ const Example = ({file, height}) => {
     const [visible, setVisible] = useState('');
     const iframe = useRef();
     const toggle = useCallback(() => setVisible(v => !v), [setVisible]);
-    const reset = useCallback(() => iframe.current.contentWindow.location.reload(), [iframe.current])
+    const reset = useCallback(() => iframe.current.contentWindow.location.reload(), [iframe.current]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && !iframe.current.src) {
+                iframe.current.src = `/frame/${file}`;
+                observer.disconnect();
+            }
+        }, {
+            rootMargin: '-60px 0px 0px 0px',
+            root: document.getElementById('app'),
+            threshold: 0,
+        });
+        observer.observe(iframe.current);
+        return () => observer.disconnect();
+    }, []);
 
     return (
         <div className='code-example'>
             <div className='live-code' style={{height}}>
-                <iframe src={`/frame/${file}`} ref={iframe}/>
+                <iframe ref={iframe}/>
                 <Controls code={js} style={scss} toggle={toggle} reset={reset}/>
             </div>
             <div className={cls('source-code', {visible})}>
