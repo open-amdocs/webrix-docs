330028098a3794482319c40fecdee69975c2d052
yoavka <yoavks@gmail.com>
2021-03-07 12:20:10 +0200

    Renamed example, updated docs

--- src/content/docs/components/movable/CustomConstraints.jsx
+++ src/content/docs/components/movable/CustomConstraints.jsx
@@ -3,7 +3,13 @@ import {Movable} from 'webrix/components';
 import './CustomConstraints.scss';
 
 const snapClass = (h, v, cls) => Movable.createConstraint({
+    // The 'dependencies' array is used internally for recreating the
+    // props when one or more elements in this array has
+    // changed (similar to how React's useCallback() dependencies work)
     dependencies: [h, v, cls],
+
+    // The below adds the class given in cls to the movable element
+    // Whenever it's on the grid provided by h & v.
     onMove: (e, {ref}, {next}) => {
         const {top, left} = next;
         ref.current.classList.remove(cls);
