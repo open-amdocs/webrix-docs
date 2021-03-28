1db5a4cd048089b11b82c30bcb91bfde818e48b0
yoavka <yoavks@gmail.com>
2021-03-29 00:02:15 +0300

    Added search to examples sidebar, increased width

--- src/routes/Examples/components/Sidebar/Sidebar.jsx
+++ src/routes/Examples/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,25 @@
+import React, {useState} from 'react';
 import {Link, useLocation} from 'react-router-dom';
-import React from 'react';
 import cls from 'classnames';
+import Search from '../Search/Search';
 import ITEMS from '../../Examples.routes';
 import './Sidebar.scss';
 
 const Sidebar = () => {
+    const [query, setQuery] = useState('');
     const {pathname} = useLocation();
+    const items = ITEMS.filter(({title, tags}) => {
+        const q = query.trim().toLowerCase();
+        return title.toLowerCase().includes(q)
+            || tags.some(tag => tag.toLowerCase().includes(q))
+    });
+
     return (
         <nav>
+            <Search value={query} onChange={setQuery}/>
             <ul>
-                {ITEMS.map(({title, path, tags}) => (
+                {items.length === 0 && <div className='no-results'>No examples found</div>}
+                {items.map(({title, path, tags}) => (
                     <li key={title} className={cls({active: pathname === path})}>
                         <Link to={path}>
                             {title}
