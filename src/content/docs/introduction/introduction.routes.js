b5d15fb33b37342ed876cd53bf47bf180de2325b
yoavka <yoavks@gmail.com>
2021-03-29 13:17:21 +0300

    Added "motivation" page

---
+++ src/content/docs/introduction/introduction.routes.js
@@ -0,0 +1,14 @@
+import {FaDownload, FaLightbulb} from 'react-icons/fa';
+
+export default [
+    {
+        title: 'Motivation',
+        path: '/motivation',
+        icon: FaLightbulb,
+    },
+    {
+        title: 'Installation',
+        path: '/installation',
+        icon: FaDownload,
+    },
+]
