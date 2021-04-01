08a5b9bf3766ddff40cd40bd8990c98db4a58f01
yoavka <yoavks@gmail.com>
2021-04-02 00:55:40 +0300

    Updated motivation readme

--- src/content/docs/introduction/motivation/Layers.jsx
+++ src/content/docs/introduction/motivation/Layers.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Layers.scss';
 
+const Glow = ({color, id}) => (
+    <filter id={id} filterUnits="userSpaceOnUse" x="-10" y="-10" width="120" height="120">
+        <feDropShadow dx="0" dy="1" stdDeviation="2" floodColor={color} floodOpacity="0.2"/>
+        <feDropShadow dx="0" dy="-1" stdDeviation="2" floodColor={color} floodOpacity="0.2"/>
+    </filter>
+);
+
 const Gradient = ({from, to, id}) => (
     <linearGradient id={id} x1='0%' y1='0%' x2='100%' y2='0%'>
         <stop offset='0%' stopColor={from} />
@@ -10,7 +17,8 @@ const Gradient = ({from, to, id}) => (
 
 export const Layer = ({text, gradient, size, offset = [0, 0, 0]}) => {
     const w = size, h = 40 * (size / 100), t = 5;
-    const id = Math.trunc(Math.random() * 10000 + 10000);
+    const gid = Math.trunc(Math.random() * 10000 + 10000);
+    const fid = Math.trunc(Math.random() * 10000 + 10000);
     return (
         <g className='layer' style={{
             '--offset-x': `${offset[0]}px`,
@@ -18,12 +26,13 @@ export const Layer = ({text, gradient, size, offset = [0, 0, 0]}) => {
             '--offset-z': `${offset[2]}px`,
             '--size': `${size}`,
         }}>
-            <path d={`M0,${h/2+t} v${-t} L${w/2},${0} L${w},${h/2} v${t} L${w/2},${h+t} Z`} fill={`url(#${id})`}/>
+            <path d={`M0,${h/2+t} v${-t} L${w/2},${0} L${w},${h/2} v${t} L${w/2},${h+t} Z`} fill={`url(#${gid})`} filter={`url(#${fid})`}/>
             <path d={`M0,${h/2+t} v${-t} L${w/2},${h} v${t} Z`}/>
             <path d={`M${w/2},${h+t} v${-t} L${w},${h/2}  v${t} Z`}/>
             <text x={0} y={0} dominantBaseline='middle' textAnchor='middle' style={{fontSize: 5 * size / 100}}>{text}</text>
             <defs>
-                <Gradient id={id} from={gradient[0]} to={gradient[1]}/>
+                <Gradient id={gid} from={gradient[0]} to={gradient[1]}/>
+                <Glow id={fid} color={gradient[0]}/>
             </defs>
         </g>
     );
