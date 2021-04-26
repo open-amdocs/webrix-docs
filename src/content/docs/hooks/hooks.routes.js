761439f9bcdae478b184ce38557034639d1c55b5
yoavka <yoavks@gmail.com>
2021-04-26 23:16:52 +0300

    Improved descriptions

--- src/content/docs/hooks/hooks.routes.js
+++ src/content/docs/hooks/hooks.routes.js
@@ -2,41 +2,41 @@ export default [
     {
         title: 'useBooleanState()',
         path: '/usebooleanstate',
-        description: 'Create a boolean value state and functions to manipulate that state.',
+        description: 'useBooleanState() is a React hook that creates a boolean value state and functions to manipulate that state.',
     },
     {
         title: 'usePrevious()',
         path: '/useprevious',
-        description: 'Get the previous value of a given variable.',
+        description: 'usePrevious() is a React hook for getting the previous value of a given variable.',
     },
     {
         title: 'useMounted()',
         path: '/usemounted',
-        description: 'Determine whether the given component has been mounted, unmounted, or neither.',
+        description: 'useMounted() is a React hook for determining whether a given component has been mounted, unmounted, or neither.',
     },
     {
         title: 'useClickOutside()',
         path: '/useclickoutside',
-        description: 'Execute a callback when clicking outside a given component, based on the React component tree rather than the DOM tree in order to support portals.',
+        description: 'useClickOutside() is a React hook for executing a callback when clicking outside a given component, based on the React component tree rather than the DOM tree in order to support portals.',
     },
     {
         title: 'useTimeout()',
         path: '/usetimeout',
-        description: 'Set a timeout (or an interval) with an easy-to-use syntax, and without worrying about memory cleanup when unmounting.',
+        description: 'useTimeout() is a React hook for setting a timeout (or an interval) with an easy-to-use syntax, and without worrying about memory cleanup when unmounting.',
     },
     {
         title: 'useDebounce()',
         path: '/usedebounce',
-        description: 'Call a function only once in a given period of time, and delay the execution based on a given delay after the last invocation.',
+        description: 'useDebounce() is a React hook for calling a function only once in a given period of time, and delay the execution based on a given delay after the last invocation.',
     },
     {
         title: 'useThrottle()',
         path: '/usethrottle',
-        description: 'Call a function only once in a given period of time, and delay the execution based on a given delay after the last invocation.',
+        description: 'useThrottle() is a React hook for calling a function only once in a given period of time, and delay the execution based on a given delay after the last invocation.',
     },
     {
         title: 'useObject()',
         path: '/useobject',
-        description: 'Return a memoized object that only changes if one or more of its values have changed.',
+        description: 'useObject() is a React hook that returns a memoized object that only changes if one or more of its values have changed.',
     },
 ];
--- src/content/docs/tools/tools.routes.js
+++ src/content/docs/tools/tools.routes.js
@@ -5,12 +5,12 @@ export default [
         title: 'ResizeObserver',
         path: '/resizeobserver',
         icon: FaWrench,
-        description: 'The <ResizeObserver/> can be used to listen to size changes in the given child component. This component uses the browser\'s ResizeObserver internally, and cleans up the memory when necessary.',
+        description: '&lt;ResizeObserver/&gt; is a React component that can be used to listen to size changes in the given child component. This component uses the browser\'s ResizeObserver internally, and cleans up the memory when necessary.',
     },
     {
         title: 'Puppeteer',
         path: '/puppeteer',
         icon: FaWrench,
-        description: '<Puppeteer/> and it\'s accompanying components/HOCs allow you to override the props of descendants\n that are decorated with the `puppet()` HOC.',
+        description: '&lt;Puppeteer/&gt; and it\'s accompanying components/HOCs allow you to override the props of descendants\n that are decorated with the `puppet()` HOC.',
     },
 ];
