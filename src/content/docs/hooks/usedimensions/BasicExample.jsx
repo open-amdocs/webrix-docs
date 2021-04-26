2ac1887f7af43379df5e30efbc9bfa0003848805
elhoucine <houcineparade@gmail.com>
2021-04-26 11:45:26 +0200

    updates

--- src/content/docs/hooks/usedimensions/BasicExample.jsx
+++ src/content/docs/hooks/usedimensions/BasicExample.jsx
@@ -2,37 +2,20 @@ import React, {useRef} from 'react';
 import {useDimensions} from 'webrix/hooks';
 import './BasicExample.scss';
 
-const getcolor = (height) => {
-    const colors = [
-        {bg: '#264653', color: '#fff'},
-        {bg: '#2a9d8f', color: '#fff'}, 
-        {bg: '#e9c46a', color: '#000'},
-        {bg: '#f4a261', color: '#000'}, 
-        {bg: '#e76f51', color: '#000'},
-    ];
-    if (height > 170) {
-        return colors[0]
-    } else if (height > 130) {
-        return colors[1]
-    } else if (height > 80) {
-        return colors[2]
-    } else if (height > 40) {
-        return colors[3]
-    }
-    return colors[4]
-}
+const getSize = height => Object.entries({
+    s: height < 40,
+    m: height >= 40 && height < 80,
+    l: height >= 80 && height < 130,
+    xl: height >= 130 && height < 170,
+}).reduce((acc, [cls, inRange]) => inRange ? cls : acc, '');
+
 export default () => {
     const myRef = useRef();
     const {width, height} = useDimensions(myRef);
-    const {bg: bgColor, color} = getcolor(height);
+    const size = getSize(height);
     return (
-            <div ref={myRef} className='container'>
-                <div className='cube' style={{
-                        height: height * 0.786,
-                        width: width * 0.786,
-                        backgroundColor: bgColor,
-                        color: color,
-                }}>{height} x {width}</div>
-            </div>
+        <div ref={myRef} className={`container ${size}`}>
+            {height} x {width}
+        </div>
     );
 };
