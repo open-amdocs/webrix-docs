b618b806a592f720094134b3296797f4b0672805
gaeaehrlich <gaeaeh@gmail.com>
2021-05-05 16:53:36 +0300

    Scroll to top when switching route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Switch, Route, useLocation} from 'react-router-dom';
 import ROUTES from './Router.routes.js';
 import {ErrorBoundary} from 'components';
 import './Router.scss';
 
-const Router = () => (
-    <Switch>
-        {ROUTES.map(({path, name, component: Comp}) => (
-            <Route path={path} key={path}>
-                <div id={`route-${name}`} className='route'>
-                    <ErrorBoundary>
-                        <Comp/>
-                    </ErrorBoundary>
-                </div>
-            </Route>
-        ))}
-    </Switch>
-);
+const Router = () => {
+    const location = useLocation();
+    useEffect(() => {
+        // If a hash exists, it indicates an external referral so the H2 will scroll into position
+        // Otherwise, no hash indicates navigation between pages, so we scroll back to top.
+        if (!location.hash) {
+            document.querySelector('.scrollbar-inner').scrollTop = 0;
+        }
+    }, [location]);
+
+    return (
+        <Switch>
+            {ROUTES.map(({path, name, component: Comp}) => (
+                <Route path={path} key={path}>
+                    <div id={`route-${name}`} className='route'>
+                        <ErrorBoundary>
+                            <Comp/>
+                        </ErrorBoundary>
+                    </div>
+                </Route>
+            ))}
+        </Switch>
+    );
+}
 
 export default Router;
