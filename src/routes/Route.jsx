d21e6e7969237cf4f97babd9fece554b8159bb62
yoavka <yoavks@gmail.com>
2021-05-11 00:24:06 +0300

    Fixed scroll to top on navigation (#37)

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,8 +1,21 @@
-import React, {Suspense, memo} from 'react';
+import React, {Suspense, memo, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import {Loader} from 'components';
 
 const Route = ({file}) => {
     const Comp = React.lazy(file);
+    const history = useHistory();
+
+    // Scroll to top when navigating between routes (excluding hash navigation)
+    useEffect(() => {
+        const unlisten = history.listen(({hash}) => {
+            if (!hash) {
+                console.log(hash);
+                document.querySelector('.route').scrollIntoView();
+            }
+        });
+        return () => unlisten();
+    }, []);
 
     return (
         <Suspense fallback={<Loader/>}>
