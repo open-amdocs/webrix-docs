4ac9ebaba22c1473255b84ee5d66c698ab2fe684
David Yushkov <fleple@users.noreply.github.com>
2021-05-24 10:11:39 +0300

    little update

--- src/routes/Docs/components/AsyncPage/AsyncPage.utils.js
+++ src/routes/Docs/components/AsyncPage/AsyncPage.utils.js
@@ -3,8 +3,6 @@ import {BASE_URL, HOOKS, INTRODUCTION} from './AsyncPage.constants';
 export const getPathToSource = ({path, title}) => {
     const type = path.split('/')[1];
     if (type === INTRODUCTION) { return null; }
-    const isHooks = type === HOOKS;
-    const name = isHooks ? title.slice(0, -2) : title;
-    const extension = isHooks ? 'js' : 'jsx';
+    const [name, extension] = type === HOOKS ? [title.slice(0, -2), 'js'] : [title, 'jsx'];
     return `${BASE_URL}/${type}/${name}/${name}.${extension}`;
 }
