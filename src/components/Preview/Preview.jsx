2b554c81808972d60773403ff1170755c1beba57
Yair Even Or <Yair.Even-Or@amdocs.com>
2022-09-29 20:09:38 +0300

    refactored the "preview" boxes

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -3,15 +3,14 @@ import {Link} from 'react-router-dom';
 import './Preview.scss';
 
 export default ({title, tags, path}) => (
-    <div className='preview-container' key={title}>
+    <Link className='link preview-container' key={title} to={path}>
         <video className='preview' src={require(`../../content${path}/preview.mp4`).default} autoPlay loop playsInline muted/>
         <div className='overlay'/>
         <div className='text'>View Example</div>
         <div className='name'>{title}</div>
         <div className='tags'>
             <div className='tag'>{tags[0]}</div>
-            {tags.length > 1 && <div className='tag'>+{tags.length - 1}</div>}
+            {tags.length > 1 && <div title={tags.slice(1).join(', ')} className='tag'>+{tags.length - 1}</div>}
         </div>
-        <Link to={path}/>
-    </div>
+    </Link>
 )
