95228ad0cc856be01d4e3c47d80ff5535500dbe7
Yair Even Or <Yair.Even-Or@amdocs.com>
2023-01-23 17:56:24 +0200

    improved example

--- src/content/examples/WheresWaldo/index.jsx
+++ src/content/examples/WheresWaldo/index.jsx
@@ -4,23 +4,22 @@ import img from './image.jpeg';
 import './style.scss';
 
 const {trackpad, update, transform} = Movable.Operations;
-const {map} = Movable.Transformers;
+const {map, clamp} = Movable.Transformers;
 
 const MIN = 0.5, MAX = 1.5;
 
 const Slider = ({value, onChange}) => {
     const movable = useRef();
-    const position = `${map(MIN, MAX, 0, 90)(value)}%`; // 90 so the handle doesn't go beyond the track
+    const position = clamp(0, 100)(map(MIN, MAX, 0, 100)(value)); // 90 so the handle doesn't go beyond the track
     const props = Movable.useMove([
         trackpad(movable),
-        transform(v => v.top, map(0, 100, MIN, MAX)),
+        transform(v => v.top, map(0, 200, MIN, MAX)),
         update(onChange),
     ]);
 
     return (
         <Movable className='slider' ref={movable} {...props}>
-            <div className='value'>{Math.round(value * 100)}%</div>
-            <div className='handle' style={{top: position}}/>
+            <div className='handle' style={{'--posY': position}}>{Math.round(value * 100)}%</div>
         </Movable>
     );
 };
@@ -31,7 +30,8 @@ export default () => {
     return (
         <div className='image-container'>
             <Slider value={scale} onChange={setScale}/>
-            <Pannable>
+            <header>{`Scale image between ${MIN*100}% and ${MAX*100}%`}</header>
+            <Pannable className='pannable-image'>
                 <Scalable scalex={scale} scaley={scale}>
                     <img src={img}/>
                 </Scalable>
